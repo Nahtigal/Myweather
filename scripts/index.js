9bb2aa980ff957da5afc2caf116f556091edb3e6
nahtigal <petryk_roman@ukr.net>
2015-12-18 09:46:17 +0000

    moments aded

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
     $(function(){
       
-     $('#btnGetWeather').click(function () {
+     $('#btnGetWeather').click(getWeatherClick);
+         
+        function getWeatherClick() {
          $('#h2').html('Шукаємо погоду для: ' + $('#city_input').val());
-    });
+    } 
+         
+         
     
     });
